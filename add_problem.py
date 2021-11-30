dfd08535f883ed6ccf3432bcfc135297884fbc98
Ramon Rodrigues <ramonrwx@gmail.com>
2021-11-30 14:41:09 -0300

    now this script copy the file from kattis

--- add_problem.py
+++ add_problem.py
@@ -1,8 +1,8 @@
 ﻿#!/usr/bin/env python
 from pathlib import Path
+from shutil import copy2
 
 KATTIS_URL = 'https://open.kattis.com/problems/'
-ROOT = Path(__file__).resolve().parents[1]
 
 success = True
 
@@ -17,12 +17,13 @@ else:
     problem = file_name
 
 if prob_title and problem:
-    file = open(ROOT.joinpath('./README.md'), 'a')
+    file = open('./README.md', 'a')
     file.write(f'| [{prob_title}]({KATTIS_URL}{problem}) | [Go](Go/{problem}.go) |\n')
 else:
     success = False
 
 if success:
+    copy2(f'./kattis/{problem}.go', './Go', follow_symlinks=True)
     print(f'The {prob_title} problem was added to the README.md')
 else:
     print('Okay, Houston, we\'ve had a problem here')
