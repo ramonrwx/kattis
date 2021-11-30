871777d0694d857f876e00ae68a22dc99aa8635b
Ramon Rodrigues <ramonrwx@gmail.com>
2021-11-30 12:40:24 -0300

    add a script to easily add a problem in the readme

---
+++ add_problem.py
@@ -0,0 +1,28 @@
+﻿#!/usr/bin/env python
+from pathlib import Path
+
+KATTIS_URL = 'https://open.kattis.com/problems/'
+ROOT = Path(__file__).resolve().parents[1]
+
+success = True
+
+prob_title = input('Enter the problem title: ')
+prob_url = input('Enter the problem URL: ')
+same_name = input('The url of the problem is the same name as the file? [Y/n] ')
+
+if same_name == '' or same_name == 'y':
+    problem = prob_url
+else:
+    file_name = input('Enter the file name: ')
+    problem = file_name
+
+if prob_title and problem:
+    file = open(ROOT.joinpath('./README.md'), 'a')
+    file.write(f'| [{prob_title}]({KATTIS_URL}{problem}) | [Go](Go/{problem}.go) |\n')
+else:
+    success = False
+
+if success:
+    print(f'The {prob_title} problem was added to the README.md')
+else:
+    print('Okay, Houston, we\'ve had a problem here')
